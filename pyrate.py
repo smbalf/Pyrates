88035041ba044535f9cef11b35e66c92e704afd6
Sam <32162434+smbalf@users.noreply.github.com>
2019-04-23 09:46:49 +0100

    update 2
    another one

--- pyrate.py
+++ pyrate.py
@@ -28,14 +28,30 @@ def get_starting_options():
         opts = (0,0,5)
     return opts
 
-def leave_port():
-    pass
+def leave_port(city_list):
+    i = 1
+    for city in city_list:
+        print("{0}) {1}".format(i, city["name"]))
+        i = i + 1
+    select_city = input("\nWhich city wish to travel to?: \n")
+    return city_list[int(select_city) - 1]
+        
 
 def buy():
     input("What would you like to buy?")
 
 def sell():
     input("What would you like to sell?")
+
+def visit_bank():
+    input("How much would you like to deposit?")
+
+cities = ({"name": "Hong Kong", "has_warehouse": True, "has_bank": True},
+            {"name": "Shanghai", "has_warehouse": False, "has_bank": False},
+            {"name": "London", "has_warehouse": False, "has_bank": False})
+
+current_city = cities[0]
+
 
 ##### START GAME #####
 welcome_message()
@@ -48,20 +64,25 @@ while game_running:
     os.system("cls")
     print(GAME_TITLE)
     print(DIVIDER)
-    print("Firm name: %s" % firm_name)
-    print("Cash: {}".format(cash))
+    print(f"Firm name: {firm_name}")
+    print(f"Cash: {cash}")
     print(f"Debt: {debt}")
-    print("Cannons: %d" % cannons)
-    print("City: [current city")
+    print(f"Cannons: {cannons}")
+    print(f"City: {current_city['name']}")
     print("Date: ")
     print(DIVIDER)
-    print("Menu: [L]eave Port, [B]uy, [S]ell, [V]isit Bank, [M]oney Lender, [Q]uit")
+    has_bank_string = ""
+    if current_city["has_bank"] == True:
+        has_bank_string = "[V]isit Bank,"
+    print("Menu: [L]eave Port, [B]uy, [S]ell, %s [T]ransfer Warehouse, [M]oney Lender, [Q]uit" % has_bank_string)
     menu_option = input("What would you like to do?: \n")
     if menu_option == "l" or "L":
-        leave_port()
+        current_city = leave_port(cities)
     elif menu_option == "b" or "B":
         buy()
     elif menu_option == "s" or "S":
         sell()
+    elif menu_option == "V" or "v" and current_city["has_bank"] == True:
+        visit_bank()
     elif menu_option == "q" or "Q":
         game_running = False
